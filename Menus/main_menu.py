de5aa32b8a4376cbaa6eaf22044f43659560f0c4
Mailine <nguyen.mailine@gmail.com>
2020-04-27 11:57:19 +0200

    clear() added

--- Menus/main_menu.py
+++ Menus/main_menu.py
@@ -1,14 +1,16 @@
 from pyfiglet import Figlet
 from Menus.menu_ouvert import Ouvert
+from FonctionBD import clear
 
 class Main_menu: 
     def __init__(self): 
         pass
     
     def Bienvenue(self):
+        clear()
         welcome = Figlet(font='big')
         print(welcome.renderText('Bienvenue'))
-        
+
     def Banner(self): 
         print("༼ つ ಥ_ಥ ༽つ ༼ つ ಥ_ಥ ༽つ ༼ つ ಥ_ಥ ༽つ ༼ つ ಥ_ಥ ༽つ ༼ つ ಥ_ಥ ༽つ")
         print("\n")
@@ -21,6 +23,7 @@ class Main_menu:
         print(bye.renderText('Beurre Miel'))
 
     def new_menu(self, content):
+        
         vue_actuelle = Ouvert(content)
         while vue_actuelle:
             vue_actuelle = vue_actuelle.run()
